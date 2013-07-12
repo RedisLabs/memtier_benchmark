fed64a635ca41a21d910796de3f468c66904f85b
Yossi Gottlieb <yossigo@gmail.com>
2013-07-12 15:27:16 +0300

    Fix license text.

--- obj_gen.h
+++ obj_gen.h
@@ -7,13 +7,13 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 2.
  *
- * Foobar is distributed in the hope that it will be useful,
+ * memtier_benchmark is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with memtier_benchmark.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _OBJ_GEN_H
